f00626d553672078b61d8eda8449b57a98212ac1
andriikrapa <andrii.krapa@gmail.com>
2022-02-17 20:48:39 +0100

    Refactoring and add function

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,29 +1,76 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
 
 const headRef = document.querySelector('head');
 const galleryRef = document.querySelector('.gallery');
-const galleryStyles = headRef.insertAdjacentHTML('beforeend',
-    `<style>
-       .imgItemList {
-           width: 100%;
-           height: 100%;
-           object-fit: cover;
-       } 
-    </style>`
-)
+const imgStyles = createImgStyles();
+const GalleryCardsMurkup = createGalleryCardsMarkup(galleryItems);
 
-function galleryMarkup(galleryItems) {
-    const markup = galleryItems
-        .map((item, index) =>
-            `<li class="list-item" name="previewImage-${index + 1}">
-            <img class="imgItemList" src="${item.preview}" alt="${item.description}">
-            </li>`)
-        .join("");
-    
-    galleryRef.insertAdjacentHTML("beforeend", markup);
+headRef.insertAdjacentHTML('beforeend', imgStyles);
+
+galleryRef.insertAdjacentHTML("beforeend", GalleryCardsMurkup);
+
+galleryRef.addEventListener('click', onImgClick);
+
+function createImgStyles() {
+    return `
+        <style>
+            .img-item {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+            } 
+        </style>
+        `;
 }
-galleryMarkup(galleryItems);
 
+function createGalleryCardsMarkup(galleryItems) {
+    return galleryItems
+        .map(({preview, original, description, }) => {
+            return `
+                <div class="img-card">
+                    <img
+                        class="img-item"
+                        src="${preview}"
+                        alt="${description}"
+                        data-original-img="${original}"
+                    >
+                </div>
+                `;
+        })
+        .join('');
+}
+
+function onImgClick(e) {
+    const isImgEl = e.target.classList.contains('img-item');
+    const selectedImg = e.target;
+
+    if (!isImgEl) {
+        return;
+    }
+    
+    removeActiveClassFromImg();
+    addActiveClassOnImg(selectedImg);
+
+    console.log(selectedImg.alt);
+}
+
+function removeActiveClassFromImg() {
+    const currentActiveImg = document.querySelector('.img-item.is-active');
+
+    if (currentActiveImg) {
+        currentActiveImg.classList.remove('is-active');
+    }
+
+    // Краткая замена предыдущему блоку.
+    // Если currentActiveImg = null || undefined правая часть не выполнится.
+    // currentActiveImg?.classList.remove('is-active');
+}
+
+function addActiveClassOnImg(selectedImg) {
+    selectedImg.classList.add('is-active')
+}
+
+//console.log()
