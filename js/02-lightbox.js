1766a23318a8f461203c16fa1443d47c4b448e75
andriikrapa <andrii.krapa@gmail.com>
2022-02-18 11:28:01 +0100

    Create markup gallery task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const GalleryCardsMurkup = createGalleryCardsMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML("beforeend", GalleryCardsMurkup);
+
+function createGalleryCardsMarkup(galleryItems) {
+    return galleryItems
+        .map(({preview, original, description, }) => {
+            return `
+                <a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a>
+                `;
+        })
+        .join('');
+}
